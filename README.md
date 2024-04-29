698baeda8cf971cc46ce38ba8f4e92818f269c13
Hasan Afrezsa <hsafrezsa97@gmail.com>
2024-04-29 16:03:37 +0700

    make script, html form, ect

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
-** This is random string generator using html css js
+## This is random string generator using html css js
 https://github.com/afrezsahs/randstringGen.git
 
-** Logs
+## Logs
 [28/04/2024] : Initiating my project 
+[29/04/2024] : basic random script 
 
-** Task 
- - [ ] scripting js;
- - [ ] basic html form and button for i/o;
+## Task 
+ - [x] basic scripting js;
+ - [x] basic html form and button for i/o;
+ - [ ] preventing undefined value;
+ - [ ] input box number only;
+ - [ ] copy button;
+ - [ ] bigger box;
+ - [-] Implemeting DOM;
  - [ ] css customisation;
--- script/script.js
+++ script/script.js
@@ -1,1 +1,50 @@
-console.log("Hi Hasan!")
+const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lower = "abcdefghijklmnopqrstuvwxyz"; 
+var numb = "0123456789";
+ const symb = "!\"#$\&\'()*+,-./<=>?@[\\]^_`{|}~";
+function getRandomInt(max) { // return rand(max)
+  return Math.floor(Math.random() * max);
+}
+
+function generatestring( many,str ) { // generate string
+result = "";
+   for (i=0;i<many;i++){ 
+x=getRandomInt(str.length);
+result += str[x] ;
+}
+
+ return result;  }
+
+ function sumstring(choiceX){
+    result = ""
+    for (i = 0; i<choiceX.length;i++){
+switch (true) {
+    case (choiceX[i]=='upper'): result += upper; break;
+    case (choiceX[i]=='lower'): result += lower; break;
+    case (choiceX[i]=='numb'): result += numb; break;
+    case (choiceX[i]=='symb'): result += symb; break;}
+ } return result;
+ }
+console.log(generatestring (10,symb+numb));
+
+
+const form = document.querySelector('form')
+form.addEventListener ('submit',(e)=>{
+    e.preventDefault();
+let choice = [];
+    document.querySelectorAll('[type="checkbox"]').forEach(item =>{
+        if (item.checked===true){
+            choice.push(item.value)
+        }
+    })
+    var test2 = form.querySelector('[id="many"]')
+    selectedVal = test2.value;
+    console.log(choice);
+    console.log(test2);
+    console.log(generatestring (10,sumstring(choice)));
+   
+    resultbox = form.querySelector('[id="result"]')
+    resultbox.value = generatestring (selectedVal,sumstring(choice));
+})
+
+//document.getElementById("demo").innerHTML = generatestring (10,test);
